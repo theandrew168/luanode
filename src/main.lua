53d554e00521d7fcf04597c825204d697b293a72
theandrew168 <steveday168@gmail.com>
2016-10-31 15:25:36 -0500

    Fix ffi path, must be ran via 'make run'

--- src/main.lua
+++ src/main.lua
@@ -3,7 +3,7 @@ ffi.cdef[[
 void ffi_init();
 ]]
 
-local gfxlib = ffi.load('/home/adail/luanode/libs/libgfx/libgfx.a')
+local gfxlib = ffi.load('libs/libgfx/libgfx.a')
 
 gfxlib.ffi_init()
 print('LuaJIT rocks!')
